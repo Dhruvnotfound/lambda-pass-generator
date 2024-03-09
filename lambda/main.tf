b92b4fc0831b5f52405581a2891b2628053096ac
Dhruvnotfound <114940443+Dhruvnotfound@users.noreply.github.com>
2024-03-09 17:07:00 +0530

    Add files via upload

---
+++ lambda/main.tf
@@ -0,0 +1,31 @@
+data "archive_file" "zip" {
+  type        = "zip"
+  source_file = "password.py"
+  output_path = "password.zip"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.policy.json
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "password_gene"
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  role    = aws_iam_role.iam_for_lambda.arn
+  handler = "password.lambda_handler"
+  runtime = "python3.12"
+}
